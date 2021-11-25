a714b63daa870c2de86ba2d4563b3b3a5b1d82b3
amansharma1205 <Drish(35&@>
2021-11-25 11:42:22 +0530

    Course TC

--- pageObjects/CreateNewCourse/CreateNewCourse_Page.js
+++ pageObjects/CreateNewCourse/CreateNewCourse_Page.js
@@ -4,22 +4,22 @@ class CreateNewCourse_Page{
     
     
    
-   get CourseTitleBTN() { return $("//span[@class='MuiIconButton-label'][position()=1]");}
+   get CourseTitleBTN() { return $("//div[2]/div[1]/div[1]/button[1]");}
    get CourseTitleTextArea() { return $("//textarea[@id='Task-Name-basic']");}
    get CourseTitleSaveBTN() { return $("//button[@class='MuiButtonBase-root MuiButton-root MuiButton-contained']/span[@class='MuiButton-label']");}
 
-   get CourseVideoPreviewBTN() { return $("//div[@class='jss30'][2]/div[@class='jss31']/button[@class='MuiButtonBase-root MuiIconButton-root jss32']");}
+   get CourseVideoPreviewBTN() { return $("//div[2]/div[2]/div[1]/button[1]");}
    get CourseVideoURL() { return $("//textarea[@id='Task-Name-basic']");}
+   get CourseVideoURLSaveBTN() { return $("//button[@class='MuiButtonBase-root MuiButton-root MuiButton-contained']/span[@class='MuiButton-label']");}
+   
+   get CourseDescriptionBTN() { return $("//div[2]/div[3]/div[1]/button[1]");}
+   get CourseTakeAwaySkill() { return $("//div[4]/div[2]/div[1]/div[1]/input[1]");}
 
-   get CourseDescriptionBTN() { return $("//div[@class='jss30'][2]/div[@class='jss31']/button[@class='MuiButtonBase-root MuiIconButton-root jss32']");}
-
-   get CourseTakeAwaySkill() { return $("//div[@class='MuiFormControl-root jss63']/div/div/input");}
-
-   get CourseTokensOnCompletion() { return $("//div[@class='MuiCardContent-root jss26']/div[5]//input[@name='checkedA']");}
-   get CourseTokenQuantity() { return $("//div[@class='MuiCardContent-root jss26']/div[5]/div[2]/div/div[1]/div/input");}
-   get CourseMemonic() { return $("//div[@class='MuiCardContent-root jss26']/div[5]/div[2]/div/div[2]/div/input");}
-   get CourseRevId() { return $("//div[@class='MuiCardContent-root jss26']/div[5]/div[2]/div/div[3]/div/input");}
-   get CourseSaveTokenDetail() { return $("//div[@class='MuiCardContent-root jss26']/div[5]/div[2]/div/button[@type='button']/span[@class='MuiButton-label']");}
+   get CourseTokensOnCompletion() { return $("//input[@name='checkedA']");}
+   get CourseTokenQuantity() { return $("//input[@type='number']");}
+   get CourseMemonic() { return $("//div[2]/div[1]/input[1]");}
+   get CourseRevId() { return $("//div[2]/div[1]/div[3]/div[1]/input[1]");}
+   get CourseSaveTokenDetail() { return $("//div[5]/div[2]/div[1]/button[1]");}
    
    get CourseExtends() { return $("//div[@id='type-simple-select' and text()='Wallet Workshop']");}
    get CourseDifficulty() { return $("//div[@id='type-simple-select' and text()='Intermediate']");}
@@ -65,15 +65,16 @@ class CreateNewCourse_Page{
          await userProfile_Page.creatorSpaceCourseDetailsPage.waitForDisplayed();
 
          await this.CourseTitleBTN.waitForDisplayed({ timeout: 20000 });
-         await this.CourseTitleBTN.click();
-        
-         
+         await this.CourseTitleBTN.click();     
          await helpers.setValue(this.CourseTitleTextArea,"Test Course");
          await this.CourseTitleSaveBTN.click();
 
          await this.CourseVideoPreviewBTN.click();
          await helpers.setValue(this.CourseVideoURL,"https://www.youtube.com/watch?v=ONleEKweKL4");
+         await this.CourseVideoURLSaveBTN.click();
 
+         await this.CourseTakeAwaySkill.scrollIntoView();
+         await this.CourseTakeAwaySkill.click();
          await this.CourseTakeAwaySkill.setValue("Coins and Crypto");
          await this.CourseTokensOnCompletion.click();
          await this.CourseTokenQuantity.setValue("5"); 
@@ -82,14 +83,14 @@ class CreateNewCourse_Page{
          await this.CourseSaveTokenDetail.click();
          
 
-
-
          await userProfile_Page.creatorSpaceLessonsPageBTN.click();
+         
          await userProfile_Page.creatorSpaceLessonsAddSectionBTN.waitForDisplayed();
          await userProfile_Page.creatorSpaceLessonsAddSectionBTN.click();
          await userProfile_Page.creatorSpaceLessonsAddSectionCancelBTN.waitForDisplayed()
          await userProfile_Page.creatorSpaceLessonsAddSectionCancelBTN.click();
          await userProfile_Page.creatorSpaceStatsPageBTN.click();
+         await browser.pause(10000);
 
     }
     
